a536b915583450ad91b94c3d405c9273e8ac66ef
Diana Peláez <dianapelaez@MacBook-Pro-de-Diana-2.local>
2019-02-01 17:43:49 -0600

    prueba2

--- src/data.js
+++ src/data.js
@@ -1,8 +1,11 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
+const filter = () => {
 };
 
-window.example = example;
+const order = () => {
+}; 
+
+window.filter = filter;
+window.order = order;
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,23 @@
 require('../src/data.js');
 
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
+// describe('example', () => {
+//   it('is a function', () => {
+//     expect(typeof example).toBe('function');
+//   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+//   it('returns `example`', () => {
+//     expect(example()).toBe('example');
+//   });
+// });
+
+describe ("filter", () => {
+  it("is a function", () => {
+    expect(typeof window.filter).toBe ("function");
   });
-});
+})
+  describe ("order", () => {
+    it("is a function", () => {
+      expect(typeof window.order).toBe ("function");
+    });
+})
